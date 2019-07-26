dd02a482012fb3178f584c1264e9954137ad0654
Stefano and ryohei <steppashr+watanaber17@gmail.com>
2019-07-26 16:37:54 +0900

    crappy clack basic completed

--- models/userMessages/create.js
+++ models/userMessages/create.js
@@ -1,5 +1,28 @@
-module.exports = (knex, UserMessage) => {
-  return (params) => {
-    return Promise.resolve({}); // fix me!
-  };
+module.exports = (knex, UserMessage) => (params) => {
+  return knex("user_messages")
+    .insert({
+      from_id: params.fromId,
+      to_id: params.toId,
+      message: params.message,
+    })
+    .then(() => {
+      return knex
+        .select(
+          "user_messages.id",
+          "user_messages.message",
+          "user_messages.sent_at",
+          "users.username as from",
+          "user_messages.to_id"
+        )
+        .from("user_messages")
+        .join("users", "user_messages.from_id", "users.id")
+        .whereIn("user_messages.from_id", [params.fromId, params.toId])
+        .whereIn("user_messages.to_id", [params.toId, params.fromId]);
+    })
+    .then((userMessageArr) => {
+      const returnedArr = userMessageArr.map((message) => {
+        return new UserMessage(message);
+      });
+      return returnedArr;
+    });
 };
--- models/userMessages/list.js
+++ models/userMessages/list.js
@@ -1,5 +1,25 @@
 module.exports = (knex, UserMessage) => {
   return (params) => {
-    return Promise.resolve([]); // fix me!
+    console.log("params in list", params);
+    return Promise.resolve(
+      knex
+        .select(
+          "user_messages.id",
+          "user_messages.message",
+          "user_messages.sent_at",
+          "users.username as from",
+          "user_messages.to_id"
+        )
+        .from("user_messages")
+        .join("users", "user_messages.from_id", "users.id")
+      //.where({ from_id: params.fromId })
+    ).then((messages) => {
+      console.log("messages in list", messages);
+      const mappedMessages = messages.map((message) => {
+        return new UserMessage(message);
+      });
+      console.log("mapped messages", mappedMessages);
+      return mappedMessages.slice(1);
+    });
   };
 };
